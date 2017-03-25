b0bf48eef2a47ec708876605928de676edb262c8
Gulchuk Petr <P.Gulchuk@yandex.ru>
2017-03-25 08:15:54 +0500

    Focuse on empty field

--- views/redux/actions/index.js
+++ views/redux/actions/index.js
@@ -13,46 +13,39 @@ export function getCity(cityFilter) {
                 type: SET_SPINNER,
                 spinner: true
             })
-        }, 0);
+        }, 500);
 
-        setTimeout(() => {
+        let promise = new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest();
+            xhr.open("get", '/source/kladr.json', true);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+            xhr.onreadystatechange = () => {
+
+                if (xhr.readyState != 4) return;
+
+                if (xhr.status != 200) {
+                    reject(xhr.status + ': ' + xhr.statusText)
+                } else {
+                    resolve(xhr.responseText);
+                }
+            };
+
+            xhr.send();
+        });
+
+        promise.then((value) => {
             dispatch({
                 type: GET_CITY_SUCCESS,
-                city: '[{"Id": 0,"City": "г. Белинский"}]'
+                city: value
             })
-        }, 2000);
-
-        // let promise = new Promise((resolve, reject) => {
-        //     let xhr = new XMLHttpRequest();
-        //     xhr.open("get", '/source/kladr.json', true);
-        //     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        //
-        //     xhr.onreadystatechange = () => {
-        //
-        //         if (xhr.readyState != 4) return;
-        //
-        //         if (xhr.status != 200) {
-        //             reject(xhr.status + ': ' + xhr.statusText)
-        //         } else {
-        //             resolve(xhr.responseText);
-        //         }
-        //     };
-        //
-        //     xhr.send();
-        // });
-        //
-        // promise.then((value) => {
-        //     dispatch({
-        //         type: GET_CITY_SUCCESS,
-        //         city: value
-        //     })
-        // }).catch((value) => {
-        //     console.error(value);
-        //     dispatch({
-        //         type: GET_CITY_ERROR,
-        //         error: true
-        //     })
-        // });
+        }).catch((value) => {
+            console.error(value);
+            dispatch({
+                type: GET_CITY_ERROR,
+                error: true
+            })
+        });
     }
 }
 
